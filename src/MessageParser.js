cf1293ebc3a5d482d4c8f6a4802ac993f3fc6bde
unknown <saad.e.alif2010@gmail.com>
2024-07-25 23:27:52 +0100

    got idea about the messageparser

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,13 +1,18 @@
 // MessageParser starter code
 class MessageParser {
     constructor(actionProvider, state) {
-      this.actionProvider = actionProvider;
-      this.state = state;
+        this.actionProvider = actionProvider;
+        this.state = state;
     }
-  
+
     parse(message) {
-      console.log(message)
+        //   console.log(message)
+        console.log(this.state);
+        const lowercase = message.toLowerCase();
+        if (lowercase.includes('hello mate')) {
+            console.log('guten tag!')
+        }
     }
-  }
-  
-  export default MessageParser;
+}
+
+export default MessageParser;
